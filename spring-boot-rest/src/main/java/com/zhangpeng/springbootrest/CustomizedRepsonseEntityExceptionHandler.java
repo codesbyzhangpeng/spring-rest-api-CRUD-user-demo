f8008a1f757c66db5c24a85d95eecaf565ad1c9f
czp <czp_182000@hotmail.com>
2019-07-23 13:40:18 +0200

    Add ExceptionHandler and global Exception Handler @ControllerAdvice and @ExceptionHandler

---
+++ spring-boot-rest/src/main/java/com/zhangpeng/springbootrest/CustomizedRepsonseEntityExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.zhangpeng.springbootrest;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomizedRepsonseEntityExceptionHandler 
+					extends ResponseEntityExceptionHandler {
+		
+	@ExceptionHandler
+	protected ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse =	new ExceptionResponse(new Date(),ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, status.NOT_FOUND);
+	}
+
+	
+	@ExceptionHandler
+	protected ResponseEntity<Object> handleAllException(Exception ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse =	new ExceptionResponse(new Date(),ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, status.INTERNAL_SERVER_ERROR);
+	}
+
+}
