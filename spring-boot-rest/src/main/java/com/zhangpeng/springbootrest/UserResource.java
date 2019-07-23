c2f8f250b55926bca7b55c4a94cefec9d68101eb
czp <czp_182000@hotmail.com>
2019-07-23 12:51:50 +0200

    UserNotFoundExcetpion -> custom exception added (with annotation: @ResponseStatus(HttpStatus.NOT_FOUND))

--- spring-boot-rest/src/main/java/com/zhangpeng/springbootrest/UserResource.java
+++ spring-boot-rest/src/main/java/com/zhangpeng/springbootrest/UserResource.java
@@ -1,11 +1,16 @@
 package com.zhangpeng.springbootrest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -20,6 +25,24 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User getOneUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user==null) {
+			throw new UserNotFoundException("The user does not exist - id = " + id);
+		}
+		
+		return user;
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> addUser(@RequestBody User user) {
+		User savedUser = service.addUser(user);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+			
+		return ResponseEntity.created(location).build();
 	}
 }
